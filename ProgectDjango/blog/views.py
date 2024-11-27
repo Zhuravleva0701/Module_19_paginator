04d07be2283dc9b4248c1be02669e7ff5ccc7138
Zhuravleva0701 <shpachevamariia@gmail.com>
2024-11-27 13:35:43 +0300

    пагинатор

--- ProgectDjango/blog/views.py
+++ ProgectDjango/blog/views.py
@@ -3,14 +3,6 @@ from django.core.paginator import Paginator
 from .models import Post
 
 
-# Create your views here.
-# def index(request):
-#     posts = Post.objects.all().order_by('-created_at')
-#     paginator = Paginator(posts, 3)
-#     page_number = request.GET.get('page')
-#     page_obj = paginator.get_page(page_number)
-#     context ={'page_obj': page_obj}
-#     return render(request, 'index.html', context)
 class BlogPage:
     per_page = 3
 
